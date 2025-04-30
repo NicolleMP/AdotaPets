2605dfb0771b908d314c0a149c3330d816a46b86
NicolleMP <nimendespoltosi46@gmail.com>
2025-04-30 14:53:00 -0300

    Finished component header structure and styling

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,62 @@
+<header>
+    <div class="logo">
+      <a href="/">🐾 AdotaPets</a>
+    </div>
+  
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/sobre">Sobre</a>
+        </li>
+        <li>
+          <a href="/contato">Contato</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  
+  <style>
+    :global(body) {
+    margin: 0;
+    padding: 0;
+    }
+
+    * {
+      text-decoration: none;
+      list-style: none;
+      border: none;
+    }
+
+    header {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding: 1rem 2rem;
+      background-color: #ff9a76;
+    }
+  
+    .logo a {
+      font-size: 1.5rem;
+      font-weight: bold;
+      color: #fff;
+    }
+  
+    nav ul {
+      display: flex;
+      gap: 1.5rem;
+    }
+  
+    nav a {
+      color: #fff;
+      font-weight: 500;
+      transition: color 0.3s;
+    }
+  
+    nav a:hover {
+      color: #ff6b3b;
+    }
+
+  </style>
