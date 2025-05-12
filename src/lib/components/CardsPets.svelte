81563a1f8db64fa2cba6a7cd47a5171d01163f2d
NicolleMP <nimendespoltosi46@gmail.com>
2025-05-12 14:42:00 -0300

    Creating a component and form page for adoption

---
+++ src/lib/components/CardsPets.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import Button from "$lib/components/Button.svelte";
+    import pet1 from "../assets/pet1.jpg"; 
+
+    export let nome;
+    export let raca;
+    export let idade;
+    export let temperamento;
+    export let porte;
+    export let saude;
+</script>
+
+<section class="pet-section">
+    <div class="infos-pet">
+        <h2>Adote o {nome}</h2>
+        <img src={pet1} alt="Foto do pet" />
+
+        <h3>Detalhes importantes:</h3>
+        <ul>
+            <li><strong>Raça:</strong> {raca}</li>
+            <li><strong>Idade:</strong> {idade}</li>
+            <li><strong>Temperamento:</strong> {temperamento}</li>
+            <li><strong>Porte:</strong> {porte}</li>
+            <li><strong>Saúde:</strong> {saude}</li>
+        </ul>
+
+        <a href="/formulario">
+            <Button>Quero Adotar</Button>
+        </a>
+    </div>
+</section>
+
+<style>
+    .pet-section {
+        display: flex;
+        justify-content: center;
+        padding: 2rem;
+    }
+
+    .infos-pet {
+        max-width: 300px;
+        width: 100%;
+        text-align: center;
+        background: #f8f8f8;
+        padding: 2rem;
+        border-radius: 10px;
+        box-shadow: 0 2px 8px rgb(0, 0, 0);
+    }
+
+    .infos-pet img {
+        max-width: 300px;
+        height: auto;
+        border-radius: 8px;
+        margin: 1rem auto;
+        display: block;
+    }
+
+    .infos-pet ul {
+        text-align: left;
+        display: inline-block;
+        margin: 1rem auto;
+        padding-left: 20px;
+    }
+
+    .infos-pet li {
+        margin-bottom: 0.5rem;
+    }
+
+    .infos-pet a {
+        display: flex;
+        align-items: center;
+        text-decoration: none;
+    }
+
+</style>
---
+++ src/routes/formulario/+page.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+    import Button from "$lib/components/Button.svelte";
+
+function enviarFormulario(event: Event) {
+        event.preventDefault();
+        alert("Formulário de adoção enviado com sucesso!");
+    }
+</script>
+
+<div class="form-container">
+    <form class="formulario" on:submit={enviarFormulario}>
+            <h3>Formulário de Adoção</h3>
+
+            <label for="nome">Nome:</label>
+            <input type="text" placeholder="Digite seu nome" required />
+
+            <label for="email">Email:</label>
+            <input type="email" placeholder="Digite seu email" required />
+
+            <label for="experiencia">Já teve animais antes?</label>
+            <select>
+                <option>Selecione</option>
+                <option>Sim</option>
+                <option>Não</option>
+            </select>
+
+            <label for="moradia">Tipo de moradia:</label>
+            <select>
+                <option>Selecione</option>
+                <option>Casa</option>
+                <option>Apartamento</option>
+                <option>Sítio/Chácara</option>
+            </select>
+
+            <label for="mensagem">Mensagem:</label>
+            <textarea
+                rows="4"
+                placeholder="Por que você quer adotar o Thor?"
+                required
+            ></textarea>
+
+            <Button type="submit">Enviar</Button>
+    </form>
+</div>
+
+<style> 
+    .formulario {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 4rem;
+    }
+
+    .formulario h3 {
+        text-align: center;
+        font-size: 1.3rem;
+        color: #333;
+    }
+
+    .formulario label {
+        font-weight: bold;
+        color: #333;
+    }
+
+    .formulario input,
+    .formulario select,
+    .formulario textarea {
+        align-items: center;
+        border-radius: 6px;
+        border: 1px solid #ccc;
+        padding: 0.5rem;
+        font-size: 1rem;
+        background-color: #fff;
+    }
+</style>
